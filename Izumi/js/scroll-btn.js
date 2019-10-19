7e8c4227ed6163d49b94dd695d287c13a4dccdfc
Holoko Yurii <golovko.jura.g@gmail.com>
2019-10-19 23:34:22 +0300

    first-commit

---
+++ Izumi/js/scroll-btn.js
@@ -0,0 +1,42 @@
+   $(document).ready(function(){
+  $('body').append('<a href="#" id="go-top"><img src="img/down-icon.png" alt=""> </a>');
+});
+
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "720") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "720") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+
+$(function() {
+ $("#go-top").scrollToTop();
+});
+
+
+
+
+$(document).ready(function(){
+      $("#menu").on("click","a", function (event) {
+          //отменяем стандартную обработку нажатия по ссылке
+          event.preventDefault();
+   
+          //забираем идентификатор бока с атрибута href
+          var id  = $(this).attr('href'),
+   
+          //узнаем высоту от начала страницы до блока на который ссылается якорь
+              top = $(id).offset().top;
+           
+          //анимируем переход на расстояние - top за 1500 мс
+          $('body,html').animate({scrollTop: top}, 500);
+      });
+  });
+  
